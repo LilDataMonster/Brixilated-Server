105c5551ba23c6740536dc7d3db548a9428b70be
David Fan <fand.kanade@gmail.com>
2021-10-04 23:21:25 -0400

    Added initial command to load default lego sets

---
+++ brixilated/lego/management/commands/init_default_lego_sets.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = 'Loads the default Lego Sets into the database'
+
+    def add_arguments(self, parser):
+        # parser.add_argument('poll_ids', nargs='+', type=int)
+        default_path = os.path.join(settings.BASE_DIR, 'lego',
+                                    'management', 'commands', 'default_lego_set.csv')
+        parser.add_argument('--lego_set_file', nargs=1, default=default_path)
+
+    def handle(self, *args, **options):
+        try:
+            df = pd.read_csv(options['lego_set_file'])
+            # TODO: process lego_set_file
+        except Exception as e:
+            raise CommandError(f'Unable to process file: {e}')
